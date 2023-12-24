e082f843e0afe115404ae0688722787b5f0f6a62
Niklas <ormenisanni80343@th-nuernberg.de>
2023-12-24 23:10:44 +0100

    now the values get saved as csv file but camera doesnt work. fuck fuck fuck fuck fuck fuck fuck fuck fuck

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -59,12 +59,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation( "androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-
+    //implementation("androidx.camera.extensions:extensions-manager:1.0.0-alpha10")
+    implementation ("androidx.camera:camera-camera2:1.3.1")
+    implementation ("androidx.camera:camera-lifecycle:1.3.1")
+    implementation ("androidx.camera:camera-core:1.3.1")
+    implementation ("androidx.camera:camera-view:1.3.1")
+    implementation ("androidx.camera:camera-extensions:1.3.1")
 
 }
--- app/src/main/java/com/example/datacollection/MainActivity.kt
+++ app/src/main/java/com/example/datacollection/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.graphics.ImageFormat
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -16,6 +17,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -30,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,7 +50,137 @@ import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileWriter
 import java.io.IOException
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import androidx.core.content.ContextCompat
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.FileProvider
+import androidx.lifecycle.LifecycleOwner
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.CameraSelector
+import androidx.core.app.ActivityCompat
+import androidx.core.view.KeyEventDispatcher.Component
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kotlin.collections.ArrayList
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
 
+class MyViewModelFactory(
+    private val application: Application,
+    private val lifecycleOwner: LifecycleOwner
+) : ViewModelProvider.Factory{
+
+    override//TODO: what the actual fuck is wrong with this shittttttttttttttt FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK
+
+    fun
+
+            <T : ViewModel?>
+
+            create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MyViewModel::class.java)) {
+            return MyViewModel(application, lifecycleOwner) as T
+        } else {
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
+
+
+private const val TAG = "CameraXExample"
+private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+
+fun takePicture(context: Context, lifecycleOwner: LifecycleOwner): String {
+    // Check camera permission
+    if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+        // Handle permission not granted
+        Log.e(TAG, "Camera permission not granted")
+        return ""
+    }
+
+    // Create output directory
+    val outputDirectory = getOutputDirectory(context)
+    val photoFile = File(
+        outputDirectory,
+        SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+    )
+
+    val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+    val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+
+    val imageCapture = ImageCapture.Builder()
+        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+        .build()
+
+    // Set up camera provider
+    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+    cameraProviderFuture.addListener({
+        // Camera provider is now guaranteed to be available
+        val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+        // Set up preview
+        /*val preview = Preview.Builder()
+            .build()
+            .also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+        */
+        try {
+            // Unbind use cases before rebinding
+            cameraProvider.unbindAll()
+
+            // Bind use cases to camera
+            cameraProvider.bindToLifecycle(
+                lifecycleOwner,
+                CameraSelector.DEFAULT_FRONT_CAMERA,
+                //preview,
+                imageCapture
+            )
+
+            // Capture image
+            imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(context),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
+                        Log.d(TAG, "Image captured: $savedUri")
+                    }
+
+                    override fun onError(exception: ImageCaptureException) {
+                        Log.e(TAG, "Error capturing image: ${exception.message}", exception)
+                    }
+                }
+            )
+        } catch (e: Exception) {
+            Log.e(TAG, "Use case binding failed", e)
+        }
+
+    }, ContextCompat.getMainExecutor(context))
+
+    return photoFile.absolutePath
+}
+
+private fun getOutputDirectory(context: Context): File {
+    val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+        File(it, context.resources.getString(R.string.app_name)).apply { mkdirs() }
+    }
+    return if (mediaDir != null && mediaDir.exists())
+        mediaDir else context.filesDir
+}
 
 
 enum class Direction{
@@ -55,8 +188,8 @@ enum class Direction{
     indicator_left,
     indicator_right
 }
-private val accelerometerReading = FloatArray(3)
-
+private val accelerometerReading = FloatArray(4)
+private var direction = 0;
 
 class CollectionService : Service(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?){
@@ -65,7 +198,9 @@ class CollectionService : Service(), SensorEventListener {
             return
         }
         if(event.sensor.type == Sensor.TYPE_ACCELEROMETER){
-            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size-1)
+            accelerometerReading[3] = direction.toFloat()
+
         }
 
     }
@@ -79,11 +214,11 @@ class CollectionService : Service(), SensorEventListener {
     }
 }
 
-//private lateinit var sensorManager: SensorManager
+
+private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: MyViewModel by viewModels()
-
+    private val viewModel: MyViewModel by viewModels() {MyViewModelFactory(application, this)}
     override fun onCreate(savedInstanceState: Bundle?) {
         var dataCollector = CollectionService()
         // 1
@@ -100,8 +235,9 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .wrapContentSize(Alignment.Center)
                         .padding(16.dp),
-                        this,
-                        viewModel
+                        context = this,
+                        viewModel,
+                        activity = this
                     )
             }
         }
@@ -110,25 +246,40 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun OrganizeFrames(modifier: Modifier = Modifier, context: Context, viewModel: MyViewModel){
+fun OrganizeFrames(modifier: Modifier = Modifier, context: Context, viewModel: MyViewModel, activity: ComponentActivity){
     var isRunning by remember { mutableStateOf(false)}
     isRunning = if(isRunning){
         RunningPage(modifier, context, viewModel)
     }else{
-        StartPage(modifier)
+        StartPage(modifier, context, activity)
     }
 }
 
-@Preview
+
 @Composable
-fun StartPage(modifier: Modifier = Modifier): Boolean {
+fun StartPage(modifier: Modifier = Modifier, context: Context, activity: ComponentActivity): Boolean {
     var isRunning by remember { mutableStateOf(false)}
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
 
     ) {
-        Button(onClick = { isRunning = true },modifier = Modifier
+        Button(onClick = {
+            if (ContextCompat.checkSelfPermission(
+                    activity,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                isRunning = true
+            } else {
+                // Request camera permission
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_REQUEST_CODE
+                )
+            }
+        },modifier = Modifier
             .weight(1f)
             .fillMaxWidth()) {
             Text(stringResource(R.string.start), fontSize = 64.sp)
@@ -171,27 +322,28 @@ fun RunningPage(modifier: Modifier = Modifier, context: Context, viewModel: MyVi
             .weight(5f)
             .fillMaxSize()
             .padding(16.dp)){
-            Button(onClick = { indicatorstate = Direction.indicator_left },modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .padding(16.dp)) {
-                Text("<-", fontSize = 64.sp)
+                Button(onClick = { indicatorstate = Direction.indicator_left },modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .padding(16.dp)) {
+                    Text("<-", fontSize = 64.sp)
 
+                }
+                Button(onClick = { indicatorstate = Direction.indicator_right},modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .padding(16.dp)) {
+                    Text("->", fontSize = 64.sp)
+                }
             }
-            Button(onClick = { indicatorstate = Direction.indicator_right},modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .padding(16.dp)) {
-                Text("->", fontSize = 64.sp)
-            }
-        }
             Button(
                 onClick = { indicatorstate = Direction.indicator_off }, modifier = Modifier
                     .weight(3f)
                     .fillMaxWidth()
                     .padding(16.dp)) {
                 Text("OFF", fontSize = 64.sp)
-            }
+                }
+        direction = indicatorstate.ordinal
             Button(onClick = {
                 isRunning = false
                 viewModel.stopDataCollection()
@@ -201,18 +353,21 @@ fun RunningPage(modifier: Modifier = Modifier, context: Context, viewModel: MyVi
                 .padding(16.dp)) {
 
                 Text(stringResource(R.string.stop), fontSize = 64.sp)
-        }
+            }
     }
     return isRunning
 }
-class MyViewModel(application: Application) : AndroidViewModel(application) {
+class MyViewModel(application: Application, private val lifecycleOwner: LifecycleOwner) : AndroidViewModel(application) {
     private val _measurements = MutableLiveData<List<FloatArray>>()
+    private val pathList = mutableListOf<String>()
     val measurements: LiveData<List<FloatArray>> get() = _measurements
 
     init {
         viewModelScope.launch {
             val measurementsList = mutableListOf<FloatArray>()
             while (true) {
+                val picturePath = takePicture(getApplication(), lifecycleOwner)
+                pathList.add(picturePath)
                 measurementsList.add(accelerometerReading.clone())
                 _measurements.postValue(measurementsList.toList())
                 delay(50)
@@ -227,16 +382,18 @@ class MyViewModel(application: Application) : AndroidViewModel(application) {
     private suspend fun saveDataToCsv() {
         withContext(Dispatchers.IO) {
             try {
-                val csvFile = File(getApplication<Application>().filesDir, "data.csv")
+                val csvFile = File("/storage/emulated/0/Documents/", "data.csv")
                 val csvWriter = FileWriter(csvFile)
 
                 // Write header
-                csvWriter.write("X,Y,Z,Indicator\n")
+                csvWriter.write("X,Y,Z,Indicator,ImagePath\n")
 
                 val measurementsCopy = ArrayList(measurements.value)
+                val pathListCopy = ArrayList(pathList)
                 // Write measurements
-                for (measurement in measurementsCopy) {
-                    csvWriter.write("${measurement[0]},${measurement[1]},${measurement[2]},${Direction}\n")//TODO print the direction in the csv file
+                for (i in 0 until measurementsCopy.size) {
+                    val measurement = measurementsCopy[i]
+                    csvWriter.write("${measurement[0]},${measurement[1]},${measurement[2]},${measurement[3]},${pathListCopy[i]}\n")
                 }
                 Log.d("DataCollectionLogging", "Data saved to CSV file. Path: ${csvFile.absolutePath}")
                 csvWriter.close()
@@ -254,4 +411,17 @@ class MyViewModel(application: Application) : AndroidViewModel(application) {
             onCleared()
         }
     }
+    class Factory(
+        private val application: Application,
+        private val lifecycleOwner: LifecycleOwner
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MyViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return MyViewModel(application, lifecycleOwner) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+
 }
